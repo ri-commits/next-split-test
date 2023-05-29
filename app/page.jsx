56b025f4b8d47d00bdd01f579e66d28f7624b19e
ri-commits <kartashourychard@gmail.com>
2023-05-29 19:55:23 +0300

    Fetching data

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
-export default function Home() {
+"use client";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log(res);
   return (
     <main>
       <h1 className="">Hello next 13</h1>
