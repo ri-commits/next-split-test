256fdd235549e3f1589c4705843dcdea2ba8c077
ri-commits <kartashourychard@gmail.com>
2023-05-29 19:12:02 +0300

    Error

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+export const metadata = {
+  title: "About page",
+};
 
 const About = () => {
+  throw new Error("failed to load");
   return <div>About page</div>;
 };
 
