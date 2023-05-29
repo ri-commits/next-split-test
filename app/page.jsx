805fa1b95d0a51aa306ad5d84d762975af386608
ri-commits <kartashourychard@gmail.com>
2023-05-29 19:37:06 +0300

    TAILWIND

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1>Hello next 13</h1>
+      <h1 className="">Hello next 13</h1>
     </main>
   );
 }
