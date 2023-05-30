043dd6ace3ca9e5e5f523a3f8998ff83e72d12a7
ri-commits <kartashourychard@gmail.com>
2023-05-30 09:36:08 +0300

    Individul movie done

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 
 const IndividualMovie = async ({ params }) => {
   const { movie } = params;
@@ -10,7 +10,20 @@ const IndividualMovie = async ({ params }) => {
   return (
     <div>
       <div>
-        <h2 className="text-4xl">{res.title}</h2>
+        <h2 className="text-2xl">{res.title}</h2>
+        <h2 className="text-lg">{res.release_date}</h2>
+        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className="text-sm bg-green-600 inline-block my-2 py-2 px-4 rounded-md">
+          {res.status}
+        </h2>
+        <Image
+          className="my-12 w-full"
+          src={image_path + res.backdrop_path}
+          width={1000}
+          height={1000}
+          priority
+        />
+        <p>{res.overview}</p>
       </div>
     </div>
   );
